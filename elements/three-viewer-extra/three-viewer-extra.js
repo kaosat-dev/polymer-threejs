f6659576d14f634ea139a001a32f7afda7d968f2
Mark Moissette <kaosat.dev@gmail.com>
2013-10-24 17:18:11 +0200

    - small cleanup in main index.html and three-viewer-extra

--- elements/three-viewer-extra/three-viewer-extra.js
+++ elements/three-viewer-extra/three-viewer-extra.js
@@ -30,31 +30,11 @@ Polymer('three-viewer-extra', {
 			link.href = imgData;
 			link.click();
 			console.log("this.$.downloadAutoLink",this.$.downloadAutoLink,"link", link);
-
-		/*
-					function fireClick(node){
-			if ( document.createEvent ) {
-				var evt = document.createEvent('MouseEvents');
-				evt.initEvent('click', true, false);
-				node.dispatchEvent(evt);	
-			} else if( document.createEventObject ) {
-				node.fireEvent('onclick') ;	
-			} else if (typeof node.onclick == 'function' ) {
-				node.onclick();	
-			}
-		}			*/
-
-
-
-
 			/*var link = document.createElement("a");
 			link.download = name;
 			link.href = imgData;
 			link.click();*/
-
-
 		}
-		
 		this.captureScreen(screenCapDone.bind(this));
 	}
 });
