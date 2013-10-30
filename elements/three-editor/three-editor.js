b83a70931b40ee59adef06537a0195ee3d228acf
Mark Moissette <kaosat.dev@gmail.com>
2013-10-30 09:07:29 +0100

    - updated three-editor to handle pointer events correctly - updated index.html  * better testing layout  * example stl models also added to three-editor's scene

--- elements/three-editor/three-editor.js
+++ elements/three-editor/three-editor.js
@@ -1,2 +1,25 @@
 Polymer('three-editor', {
+
+  enteredView:function()
+  {
+    this.super();
+    console.log("editor entered view");
+    /*    
+     #if mousedown PLUS move NOT over object : rotate/pan, do not deselect current obect
+      
+      ###TRANSFORM CONTROLS###
+      @transformControls = new THREE.TransformControls(@camera, @renderer.domElement)
+      #@transformControls.scale = 0.65
+      
+      onControlsChange=(ev)=>
+        @controls.enabled = true
+        if @transformControls.axis then @controls.enabled = false
+        #if (editor.selected) signals.objectChanged.dispatch( editor.selected )
+        @onObjectSelected({type:'selected',selection:@selectionHelper.currentSelect} )
+
+        @_render()
+        
+      @transformControls.addEventListener( 'change', onControlsChange)
+      @on("selectionChange",@_onSelectionChange)*/
+  }
   });
