916d71565b1a5ffe0f566d7b6007989386ac84df
Mark Moissette <kaosat.dev@gmail.com>
2013-10-31 11:13:49 +0100

    experimenting with lighting and materials, in order to get a good default

--- elements/three-viewer/three-viewer.js
+++ elements/three-viewer/three-viewer.js
@@ -114,17 +114,18 @@ Polymer('three-viewer', {
 		setupLights: function()
 		{
 			mainScene = 	this.scene
-		  	pointLight = new THREE.PointLight(0x333333,4)
+		  	pointLight = new THREE.PointLight(0x333333,3)
 		  	pointLight.position.x = -2500
 		  	pointLight.position.y = -2500
 		  	pointLight.position.z = 2200
 			  
-		  	pointLight2 = new THREE.PointLight(0x333333,3)
+		  	pointLight2 = new THREE.PointLight(0x333333,2)
 		  	pointLight2.position.x = 2500
 		  	pointLight2.position.y = 2500
 		  	pointLight2.position.z = -5200
 			
 		  	ambientColor = 0x565595
+        ambientColor = 0x161515
 		  	ambientLight = new THREE.AmbientLight(ambientColor)
 			  
     /*
@@ -143,7 +144,7 @@ Polymer('three-viewer', {
 			spotLight.castShadow = true*/
 
       var SHADOW_MAP_WIDTH = 4096, SHADOW_MAP_HEIGHT = 2048;
-      spotLight = new THREE.SpotLight( 0xbbbbbb, 1.5, 0, Math.PI, 1 );
+      spotLight = new THREE.SpotLight( 0xbbbbbb, 0.8, 0, Math.PI, 1 );
 			spotLight.position.set( 20, 20, 250 );
 			spotLight.target.position.set( 0, 0, 0 );
 
@@ -160,8 +161,55 @@ Polymer('three-viewer', {
 
 			spotLight.shadowMapWidth = SHADOW_MAP_WIDTH;
 			spotLight.shadowMapHeight = SHADOW_MAP_HEIGHT;
+
+
+      //sky color ground color intensity 
+      hemiLight = new THREE.HemisphereLight( 0x0000ff, 0x00ff00, 0.6 );
+      hemiLight = new THREE.HemisphereLight( 0xffEEEE, 0xffEEEE, 0.2 );
+				/*hemiLight.color.setHSV( 0.8, 0.25, 1 );
+				hemiLight.groundColor.setHSV( 0.095, 0.2, 1 );*/
+				hemiLight.position.set( 0, 1200, 5000 );
 			  
-			lights = [ambientLight,pointLight, pointLight2, spotLight]
+      
+      dirLight = new THREE.DirectionalLight( 0xffffff, 0.2 );
+				//dirLight.color.setHSV( 0.1, 0.1, 1 );
+				dirLight.position.set( 0, 1.75, 200 );
+				//dirLight.position.multiplyScalar( 50 );
+
+				dirLight.castShadow = true;
+
+				dirLight.shadowMapWidth = 2048;
+				dirLight.shadowMapHeight = 2048;
+
+				var d = 50;
+
+				dirLight.shadowCameraLeft = -d;
+				dirLight.shadowCameraRight = d;
+				dirLight.shadowCameraTop = d;
+				dirLight.shadowCameraBottom = -d;
+
+				dirLight.shadowCameraFar = 3500;
+				dirLight.shadowBias = -0.0001;
+				dirLight.shadowDarkness = 0.35;
+        dirLight.onlyShadow = true;
+
+
+      //3 point lighting test
+      var pLigthIntensity = 0.5;
+      var pLight = new THREE.DirectionalLight( 0xfcfc7e, pLigthIntensity );
+      pLight.position.set( 100, 150, 200 );
+      pLight.target.position.set(0,0,0);
+
+      var pLight2 = new THREE.DirectionalLight( 0xfcfc7e, pLigthIntensity );
+      pLight2.position.set( 100, -150, 200 );
+      pLight2.target.position.set(0,0,0);
+
+      var pLight3 = new THREE.DirectionalLight( 0x86f4eb, pLigthIntensity );
+      pLight3.position.set( -100, 0, -200 );
+      pLight3.target.position.set(0,0,0);
+
+
+			lights = [ambientLight,hemiLight,dirLight,pLight,pLight2]
 			mainScene.lights = lights
 			for (var i=0; i<lights.length; i++)
 			{
